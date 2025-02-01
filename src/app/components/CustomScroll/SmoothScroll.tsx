a3470496bc2e1de46239f24f7f08022dfd232aa4
Formasitchijoh <formasitf@gmail.com>
2025-02-01 11:55:54 +0100

    fix:removed smooth attribute from lenis

--- src/app/components/CustomScroll/SmoothScroll.tsx
+++ src/app/components/CustomScroll/SmoothScroll.tsx
@@ -1,13 +1,12 @@
 'use client'
 import { useEffect } from "react";
-import Lenis, { easeInOutCubic } from "@studio-freight/lenis";
+import Lenis from "@studio-freight/lenis";
 
 const SmoothScroll = () => {
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1.2, // Adjust for slower/faster scroll
       smoothWheel: true,
-      easing: easeInOutCubic,
     });
 
     function raf(time:any) {
